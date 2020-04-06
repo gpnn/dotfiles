515b040348fb1d63017459aef1fdfb2d3b4f5b0d
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-04-06 18:30:01 -0400

    fix: fix readability in fix node-gyp script

--- fix-node-gyp.sh
+++ fix-node-gyp.sh
@@ -1,30 +1,28 @@
 XCODE="$(xcode-select --print-path)"
 
-echo "Will try resetting xcode first..."
+printf "\nWill try resetting xcode first\n\n"
 read -n 1 -s -r -p "Press any key to continue"
 xcode-select --reset
-echo
-echo
-echo "Try running the npm command again in a different terminal shell."
+printf "\n\n\n"
+printf "Try running the npm command again in a different terminal shell.\n\n"
 read -n 1 -s -r -p "Press any key to continue if it did not solve the problem"
 
 if [ -d "$XCODE" ]; then
-    echo "xcode exists at path $XCODE"
-    echo "Will delete..."
+    printf "\n\n\n"
+    printf "xcode exists at path $XCODE \n\n"
+    printf "Will delete\n"
     read -n 1 -s -r -p "Press any key to continue"
+    printf "\n\n\n"
+    printf "Deleting, please wait\n\n"
     rm -rf "$XCODE"
 fi
 
-echo
-echo
-echo "Will prompt to install xcode now"
+printf "\n\n\n"
+printf "Will prompt to install xcode now\n\n"
 read -n 1 -s -r -p "Press any key to continue"
 xcode-select --install
-echo
-echo
+printf "\n\n\n"
 read -n 1 -s -r -p "Press any key when xcode is done installing"
-echo
-echo
-echo "Installing latest npm and latest node-gyp"
-npm install -g npm@latest
-npm install -g node-gyp@latest
+printf "\n\n\n"
+printf "Installing latest npm and latest node-gyp\n\n"
+npm install -g npm@latest node-gyp@latest
