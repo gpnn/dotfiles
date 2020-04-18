45cacefc6f299a38a1e3e9fe388ef4c04ee9c10d
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-04-18 01:02:26 -0400

    fix: mostly fix color output for update shell script

--- update-plugins.sh
+++ update-plugins.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-RED='\033[1;33m'
+RED='\033[0;31m'
 NC='\033[0m'
 
 GIT_REPOS=(
-    $HOME/.nano/nanorc
-    $HOME/.oh-my-zsh
-    $HOME/.oh-my-zsh/custom/plugins/fzf-tab
-    $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-    $HOME/.oh-my-zsh/custom/plugins/zsh-better-npm-completion
-    $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-	$HOME/.oh-my-zsh/custom/themes/powerlevel10k
+	"$HOME/.nano/nanorc"
+	"$HOME/.oh-my-zsh"
+	"$HOME/.oh-my-zsh/custom/plugins/fzf-tab"
+	"$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+	"$HOME/.oh-my-zsh/custom/plugins/zsh-better-npm-completion"
+	"$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+	"$HOME/.oh-my-zsh/custom/themes/powerlevel10k"
 )
 
 for directory in "${GIT_REPOS[@]}"; do
-    echo ${RED}
-    echo "Updating $directory"
-    echo ${NC}
-    cd "$directory"
+    echo -e "$RED"
+    echo -e "Updating $directory"
+    echo -e "$NC"
+    cd "$directory" || return
     git pull origin master
 done
