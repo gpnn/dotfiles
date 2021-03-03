dad929d84e6b4042934adef4bf193bb0f6e900f5
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2021-03-03 09:50:22 -0500

    feat: add pipx plugin

--- download-zsh-plugins.sh
+++ download-zsh-plugins.sh
@@ -14,3 +14,4 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ~ZSH_CUSTOM/plugins/z
 git clone https://github.com/zsh-users/zsh-completions ~ZSH_CUSTOM/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-history-substring-search ~ZSH_CUSTOM/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/thuandt/zsh-pipx.git ~ZSH_CUSTOM/plugins/zsh-pipx
