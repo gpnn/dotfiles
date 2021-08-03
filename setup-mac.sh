2c6208bf909550c2c692ce31aba51d00c1e7e8c6
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2021-08-03 10:36:22 -0400

    chore: initial migration to zinit

--- setup-mac.sh
+++ setup-mac.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ "$MACHINE" != "Mac" ]]; then
-  echo "$MACHINE is not supported"
-  exit 1
+	echo "$MACHINE is not supported"
+	exit 1
 fi
 
 echo "$MACHINE is supported"
@@ -21,8 +21,8 @@ mkdir -p "$HOME/workspace"
 sudo true
 
 if [[ $EUID -gt 0 ]]; then
-  echo "Please run with sudo"
-  exit
+	echo "Please run with sudo"
+	exit
 fi
 
 chsh -s /usr/local/bin/zsh
@@ -30,37 +30,35 @@ chsh -s /usr/local/bin/zsh
 bash .macos
 
 download_chrome_extensions() {
-  git clone https://github.com/Sainan/Universal-Bypass.git "$HOME/chrome-extensions/Universal-Bypass"
-  git clone https://github.com/iamadamdev/bypass-paywalls-chrome.git "$HOME/chrome-extensions/bypass-paywalls-chrome"
-  git clone https://github.com/Ibit-to/google-unlocked.git "$HOME/chrome-extensions/google-unlocked"
+	git clone https://github.com/Sainan/Universal-Bypass.git "$HOME/chrome-extensions/Universal-Bypass"
+	git clone https://github.com/iamadamdev/bypass-paywalls-chrome.git "$HOME/chrome-extensions/bypass-paywalls-chrome"
+	git clone https://github.com/Ibit-to/google-unlocked.git "$HOME/chrome-extensions/google-unlocked"
 }
 
 create_symlinks() {
-  DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-  for file in "$DIR"/dotfiles/.*; do
-    if [ -d "$file" ]; then
-      continue
-    fi
-    file_name="${file##*/}"
-    ln -sfn "$file" "$HOME/$file_name"
-  done
+	DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+	for file in "$DIR"/dotfiles/.*; do
+		if [ -d "$file" ]; then
+			continue
+		fi
+		file_name="${file##*/}"
+		ln -sfn "$file" "$HOME/$file_name"
+	done
 }
 
 symlink_iterm_settings() {
-  DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-  for file in "$DIR"/iterm2/*; do
-    if [ -d "$file" ]; then
-      continue
-    fi
-    file_name="${file##*/}"
-    ln -sfn "$file" "$HOME/Library/Preferences/$file_name"
-  done
+	DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+	for file in "$DIR"/iterm2/*; do
+		if [ -d "$file" ]; then
+			continue
+		fi
+		file_name="${file##*/}"
+		ln -sfn "$file" "$HOME/Library/Preferences/$file_name"
+	done
 }
 
 download_chrome_extensions
 create_symlinks
 symlink_iterm_settings
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
-source download-zsh-plugins.sh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
