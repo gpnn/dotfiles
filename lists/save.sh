1adaa9d7191d6a3e166f16ead91f6f8d46716f69
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2022-11-23 09:57:05 -0500

    chore: update lists
    * adjust script to include pnpm global packages

--- lists/save.sh
+++ lists/save.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+pnpm_marker="/node_modules/.pnpm/"
+
 if [ -f "Brewfile" ]; then
 	rm "Brewfile"
 fi
 npm root -g
 npm ls -g --depth=0 --parseable | grep -o 'node_modules/.*$' | sed 's:node_modules/::g' >npm-global
 brew bundle dump --all
+pnpm ls -g --parseable --depth 0 | grep -i "$pnpm_marker" | awk -F"$pnpm_marker" '{print $2}' | awk -F'@' '{print $1}' >pnpm-global
